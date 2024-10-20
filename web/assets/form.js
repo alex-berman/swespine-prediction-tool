834b08234a6dafa19e31f68ec13695f93379eed1
Alex Berman <alexander.berman@gu.se>
2024-10-20 13:36:00 +0200

    WIP Add registration form

---
+++ web/assets/form.js
@@ -0,0 +1,113 @@
+import { dataModel, getDataDefinition } from "./data_model.js";
+import { tabs, getLayoutField } from "./tool_form_layout.js";
+
+export function createFormFieldWidget(field) {
+  const groupDiv = document.createElement('div');
+  groupDiv.className = 'form-group';
+
+  const label = document.createElement('label');
+  label.setAttribute('for', field.name);
+  label.className = 'label';
+  label.innerHTML = field.label;
+  groupDiv.appendChild(label);
+
+  if(field.type == 'select') {
+    const select = document.createElement('select');
+    select.id = field.name;
+    select.className = 'input';
+    for(let i = 0; i < field.labels.length; i++) {
+      const option = document.createElement('option');
+      option.text = field.labels[i];
+      option.value = i;
+      select.appendChild(option);
+    }
+    groupDiv.appendChild(select);
+  }
+  else if(field.type == 'radio') {
+    const div = document.createElement('div');
+    div.classList.add('input');
+    div.classList.add('radio-group');
+    for(let i = 0; i < field.labels.length; i++) {
+      const input = document.createElement('input');
+      input.type = 'radio';
+      input.id = field.name + i;
+      input.name = field.name;
+      input.value = i;
+      div.appendChild(input);
+
+      const label = document.createElement('label');
+      label.setAttribute('for', input.id);
+      label.innerHTML = field.labels[i];
+      div.appendChild(label);
+    }
+    groupDiv.appendChild(div);
+  }
+  else if(field.type == 'toggle') {
+    const div = document.createElement('div');
+    div.classList.add('input');
+    div.classList.add('radio-group');
+    for(let i = 1; i >= 0; i--) {
+      const input = document.createElement('input');
+      input.type = 'radio';
+      input.id = field.name + i;
+      input.name = field.name;
+      input.value = i;
+      div.appendChild(input);
+
+      const label = document.createElement('label');
+      label.setAttribute('for', input.id);
+      label.innerHTML = field.labels[i];
+      div.appendChild(label);
+    }
+    groupDiv.appendChild(div);
+  }
+  else if(field.type == 'number') {
+    const valueElement = document.createElement('input');
+    valueElement.type = 'text';
+    valueElement.inputmode = 'numeric';
+    valueElement.id = field.name + '-value';
+    valueElement.className = 'input';
+
+    const rangeElement = document.createElement('input');
+    rangeElement.type = 'range';
+    rangeElement.id = field.name;
+    const dataDefinition = getDataDefinition(field.name);
+    rangeElement.min = dataDefinition.range.min;
+    rangeElement.max = dataDefinition.range.max;
+    rangeElement.step = dataDefinition.range.step;
+    rangeElement.className = 'input';
+    rangeElement.addEventListener('input', () => {
+        valueElement.value = rangeElement.value;
+    });
+
+    valueElement.addEventListener('input', () => {
+        let parsedValue = parseFloat(valueElement.value);
+        if (parsedValue < dataDefinition.range.min) {
+            parsedValue = dataDefinition.range.min;
+        } else if (parsedValue > dataDefinition.range.max) {
+            parsedValue = dataDefinition.range.max;
+        }
+        rangeElement.value = parsedValue;
+    });
+
+    groupDiv.appendChild(valueElement);
+    groupDiv.appendChild(rangeElement);
+  }
+  else {
+    throw new Error('Unsupported field type ' + field.type);
+  }
+  return groupDiv;
+}
+
+export function initializeForm(id) {
+  const container = document.getElementById(id);
+
+  for(const tab of tabs) {
+    for(const group of tab.groups) {
+      for(const field of group.fields) {
+        const widget = createFormFieldWidget(field);
+        container.appendChild(widget);
+      }
+    }
+  }
+}
--- web/prediction_tool.js
+++ web/prediction_tool.js
@@ -1,5 +1,6 @@
 import { dataModel, getDataDefinition } from "./assets/data_model.js";
 import { tabs, getLayoutField } from "./assets/tool_form_layout.js";
+import { createFormFieldWidget } from "./assets/form.js";
 import { mean_disc_herniation } from "./assets/models/mean_disc_herniation.js";
 import { outcome_disc_herniation_coefs } from "./assets/models/outcome_disc_herniation_coefs.js";
 import { satisfaction_disc_herniation_coefs } from "./assets/models/satisfaction_disc_herniation_coefs.js";
@@ -227,101 +228,8 @@ function initializeForm() {
       tabDiv.appendChild(h);
 
       for(const field of group.fields) {
-        const groupDiv = document.createElement('div');
-        groupDiv.className = 'form-group';
-
-        const label = document.createElement('label');
-        label.setAttribute('for', field.name);
-        label.className = 'label';
-        label.innerHTML = field.label;
-        groupDiv.appendChild(label);
-
-        if(field.type == 'select') {
-          const select = document.createElement('select');
-          select.id = field.name;
-          select.className = 'input';
-          for(let i = 0; i < field.labels.length; i++) {
-            const option = document.createElement('option');
-            option.text = field.labels[i];
-            option.value = i;
-            select.appendChild(option);
-          }
-          groupDiv.appendChild(select);
-        }
-        else if(field.type == 'radio') {
-          const div = document.createElement('div');
-          div.classList.add('input');
-          div.classList.add('radio-group');
-          for(let i = 0; i < field.labels.length; i++) {
-            const input = document.createElement('input');
-            input.type = 'radio';
-            input.id = field.name + i;
-            input.name = field.name;
-            input.value = i;
-            div.appendChild(input);
-
-            const label = document.createElement('label');
-            label.setAttribute('for', input.id);
-            label.innerHTML = field.labels[i];
-            div.appendChild(label);
-          }
-          groupDiv.appendChild(div);
-        }
-        else if(field.type == 'toggle') {
-          const div = document.createElement('div');
-          div.classList.add('input');
-          div.classList.add('radio-group');
-          for(let i = 1; i >= 0; i--) {
-            const input = document.createElement('input');
-            input.type = 'radio';
-            input.id = field.name + i;
-            input.name = field.name;
-            input.value = i;
-            div.appendChild(input);
-
-            const label = document.createElement('label');
-            label.setAttribute('for', input.id);
-            label.innerHTML = field.labels[i];
-            div.appendChild(label);
-          }
-          groupDiv.appendChild(div);
-        }
-        else if(field.type == 'number') {
-          const valueElement = document.createElement('input');
-          valueElement.type = 'text';
-          valueElement.inputmode = 'numeric';
-          valueElement.id = field.name + '-value';
-          valueElement.className = 'input';
-
-          const rangeElement = document.createElement('input');
-          rangeElement.type = 'range';
-          rangeElement.id = field.name;
-          const dataDefinition = getDataDefinition(field.name);
-          rangeElement.min = dataDefinition.range.min;
-          rangeElement.max = dataDefinition.range.max;
-          rangeElement.step = dataDefinition.range.step;
-          rangeElement.className = 'input';
-          rangeElement.addEventListener('input', () => {
-              valueElement.value = rangeElement.value;
-          });
-
-          valueElement.addEventListener('input', () => {
-              let parsedValue = parseFloat(valueElement.value);
-              if (parsedValue < dataDefinition.range.min) {
-                  parsedValue = dataDefinition.range.min;
-              } else if (parsedValue > dataDefinition.range.max) {
-                  parsedValue = dataDefinition.range.max;
-              }
-              rangeElement.value = parsedValue;
-          });
-
-          groupDiv.appendChild(valueElement);
-          groupDiv.appendChild(rangeElement);
-        }
-        else {
-          throw new Error('Unsupported field type ' + field.type);
-        }
-        tabDiv.appendChild(groupDiv);
+        const widget = createFormFieldWidget(field);
+        tabDiv.appendChild(widget);
       }
     }
 
